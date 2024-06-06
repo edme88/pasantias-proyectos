7d5c688c736460da3489e919719427cdb89e9079
EXB04891 <agustina.aliciardi@darwoft.com>
2024-06-06 17:13:45 -0300

    First version

---
+++ students.js
@@ -0,0 +1,42 @@
+const students = [
+    {
+        "tema": "¿Dónde comer?",
+        "autor": ["Valentina Apellido"],
+        "anio": "2024",
+        "mes": "Junio",
+        "repositorio": "https://github.com/valen274/lugaresparacomer",
+        "url": "https://valen274.github.io/lugaresparacomer/index.html",
+        "imagen": "underConstruction.jpg",
+        "escuela": "Nombre de la Escuela"
+    },
+    {
+        "tema": "¿Qué mirar?",
+        "autor": ["Felicitas Acosta"],
+        "anio": "2024",
+        "mes": "Junio",
+        "repositorio": "https://github.com/felicitaslaje/-No-sabes-qu-ver-",
+        "url": "https://felicitaslaje.github.io/-No-sabes-qu-ver-/Holamundo-pasantia.html",
+        "imagen": "underConstruction.jpg",
+        "escuela": "Nombre de la Escuela"
+    },
+    {
+        "tema": "Oficios.com",
+        "autor": ["Franco Fontana"],
+        "anio": "2024",
+        "mes": "Junio",
+        "repositorio": "https://github.com/FrancoFontana9/Oficios.com",
+        "url": "https://francofontana9.github.io/Oficios.com/Oficios.html",
+        "imagen": "underConstruction.jpg",
+        "escuela": "Nombre de la Escuela"
+    },
+    {
+        "tema": "Reserva tu Gym",
+        "autor": ["Agustin Acosta"],
+        "anio": "2024",
+        "mes": "Junio",
+        "repositorio": "https://github.com/AgusAcosta18/Reserva-tu-horario-de-gimnacio",
+        "url": "https://agusacosta18.github.io/Reserva-tu-horario-de-gimnacio/indice.html",
+        "imagen": "underConstruction.jpg",
+        "escuela": "Nombre de la Escuela"
+    }
+]
